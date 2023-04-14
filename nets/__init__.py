92cee2132fa18667d6cc5cbefc61623034049311
YsanZyh <18813012096@163.com>
2023-04-14 17:28:23 +0800

    add.

---
+++ nets/__init__.py
@@ -0,0 +1,19 @@
+#
+def hourglass():
+    return None
+
+
+def center_training():
+    return None
+
+
+def resnet():
+    return None
+
+
+def yolo3():
+    return None
+
+
+def yolo4():
+    return None
